4028ab3e0544a7b793b332beae114aabe7b5967b
Daniel Speichert <daniel@speichert.pl>
2014-08-28 06:10:13 +0200

    Meteor 0.9.0 compatibility

--- package.js
+++ package.js
@@ -1,10 +1,16 @@
+Package.describe({
+  summary: 'Automatically shows a NProgress.js bar during DDP data exchange',
+  version: '0.2.9',
+});
+
 Package.on_use(function (api) {
-    api.use('nprogress');
-    api.add_files('auto-nprogress.js', 'client');
+  api.versionsFrom('0.9.0');
+  api.use('mrt:nprogress@0.0.5');
+  api.add_files('auto-nprogress.js', 'client');
 });
 
 Package.on_test(function (api) {
-  api.use(["tinytest", "test-helpers", "nprogress"], "client");
-  api.add_files('tests/auto-nprogress-test.js', ['client', 'server'] );
+  api.use(['tinytest', 'test-helpers', 'mrt:nprogress@0.0.5'], 'client');
+  api.add_files('tests/auto-nprogress-test.js');
   api.add_files('auto-nprogress.js', 'client');
 });
